7d186cc035c8bc5661061127822a5ca5f132672c
ShareefAbdullah <Shareef4815@gmail.com>
2025-01-21 06:00:59 +0300

    fix the links to the pages

--- script.js
+++ script.js
@@ -1,64 +1,64 @@
 const entj = document.getElementById("entj");
 entj.addEventListener("click", () => {
-    window.location = "./entj.html"
+    window.location = "./types/entj.html"
 });
 const intj = document.getElementById("intj");
 intj.addEventListener("click", () => {
-    window.location = "./intj.html"
+    window.location = "./types/intj.html"
 });
 const entp = document.getElementById("entp");
 entp.addEventListener("click", () => {
-    window.location = "./entp.html"
+    window.location = "./types/entp.html"
 });
 const intp = document.getElementById("intp");
 intp.addEventListener("click", () => {
-    window.location = "./intp.html"
+    window.location = "./types/intp.html"
 });
 const enfj = document.getElementById("enfj");
 enfj.addEventListener("click", () => {
-    window.location = "./enfj.html"
+    window.location = "./types/enfj.html"
 });
 const infj = document.getElementById("infj");
 infj.addEventListener("click", () => {
-    window.location = "./infj.html"
+    window.location = "./types/infj.html"
 });
 const enfp = document.getElementById("enfp");
 enfp.addEventListener("click", () => {
-    window.location = "./enfp.html"
+    window.location = "./types/enfp.html"
 });
 const infp = document.getElementById("infp");
 infp.addEventListener("click", () => {
-    window.location = "./infp.html"
+    window.location = "./types/infp.html"
 });
 const estj = document.getElementById("estj");
 estj.addEventListener("click", () => {
-    window.location = "./estj.html"
+    window.location = "./types/estj.html"
 });
 const istj = document.getElementById("istj");
 istj.addEventListener("click", () => {
-    window.location = "./istj.html"
+    window.location = "./types/istj.html"
 });
 const esfj = document.getElementById("esfj");
 esfj.addEventListener("click", () => {
-    window.location = "./esfj.html"
+    window.location = "./types/esfj.html"
 });
 const isfj = document.getElementById("isfj");
 isfj.addEventListener("click", () => {
-    window.location = "./isfj.html"
+    window.location = "./types/isfj.html"
 });
 const estp = document.getElementById("estp");
 estp.addEventListener("click", () => {
-    window.location = "./estp.html"
+    window.location = "./types/estp.html"
 });
 const istp = document.getElementById("istp");
 istp.addEventListener("click", () => {
-    window.location = "./istp.html"
+    window.location = "./types/istp.html"
 });
 const esfp = document.getElementById("esfp");
 esfp.addEventListener("click", () => {
-    window.location = "./esfp.html"
+    window.location = "./types/esfp.html"
 });
 const isfp = document.getElementById("isfp");
 isfp.addEventListener("click", () => {
-    window.location = "./isfp.html"
+    window.location = "./types/isfp.html"
 });
