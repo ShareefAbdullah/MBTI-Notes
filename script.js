4116f952cb6aa7d6161b223505a26ded2462c4d0
ShareefAbdullah <Shareef4815@gmail.com>
2025-01-21 05:38:46 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const entj = document.getElementById("entj");
+entj.addEventListener("click", () => {
+    window.location = "./entj.html"
+});
+const intj = document.getElementById("intj");
+intj.addEventListener("click", () => {
+    window.location = "./intj.html"
+});
+const entp = document.getElementById("entp");
+entp.addEventListener("click", () => {
+    window.location = "./entp.html"
+});
+const intp = document.getElementById("intp");
+intp.addEventListener("click", () => {
+    window.location = "./intp.html"
+});
+const enfj = document.getElementById("enfj");
+enfj.addEventListener("click", () => {
+    window.location = "./enfj.html"
+});
+const infj = document.getElementById("infj");
+infj.addEventListener("click", () => {
+    window.location = "./infj.html"
+});
+const enfp = document.getElementById("enfp");
+enfp.addEventListener("click", () => {
+    window.location = "./enfp.html"
+});
+const infp = document.getElementById("infp");
+infp.addEventListener("click", () => {
+    window.location = "./infp.html"
+});
+const estj = document.getElementById("estj");
+estj.addEventListener("click", () => {
+    window.location = "./estj.html"
+});
+const istj = document.getElementById("istj");
+istj.addEventListener("click", () => {
+    window.location = "./istj.html"
+});
+const esfj = document.getElementById("esfj");
+esfj.addEventListener("click", () => {
+    window.location = "./esfj.html"
+});
+const isfj = document.getElementById("isfj");
+isfj.addEventListener("click", () => {
+    window.location = "./isfj.html"
+});
+const estp = document.getElementById("estp");
+estp.addEventListener("click", () => {
+    window.location = "./estp.html"
+});
+const istp = document.getElementById("istp");
+istp.addEventListener("click", () => {
+    window.location = "./istp.html"
+});
+const esfp = document.getElementById("esfp");
+esfp.addEventListener("click", () => {
+    window.location = "./esfp.html"
+});
+const isfp = document.getElementById("isfp");
+isfp.addEventListener("click", () => {
+    window.location = "./isfp.html"
+});
